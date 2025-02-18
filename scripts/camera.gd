50446c854056095d0db7573546705fb74db301be
Star2578 <nuttykingger@gmail.com>
2025-02-18 07:48:55 +0700

    Setup camera

---
+++ scripts/camera.gd
@@ -0,0 +1,25 @@
+extends Camera3D
+
+@export var edge_area: float = 50
+@export var max_rotation_speed: float = 2.0
+@export var deadzone_width: float = 200
+
+func _physics_process(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var viewport_size = get_viewport().size
+	
+	var left_deadzone = viewport_size.x / 2 - deadzone_width / 2
+	var right_deadzone = viewport_size.x / 2 + deadzone_width / 2
+	
+	var rotation_speed = 0.0
+	
+	if mouse_pos.x < left_deadzone:
+		# Mouse is in the left rotation area
+		rotation_speed = max_rotation_speed * (1 - (mouse_pos.x / left_deadzone))
+	elif mouse_pos.x > right_deadzone:
+		# Mouse is in the right rotation area
+		rotation_speed = -max_rotation_speed * (1 - ((viewport_size.x - mouse_pos.x) / (viewport_size.x - right_deadzone)))
+	
+	rotate_y(deg_to_rad(rotation_speed))
+	
+	rotation_degrees.y = clamp(rotation_degrees.y, -60, 60)
